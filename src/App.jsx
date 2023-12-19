a212b1baba69e09abc3ce51d68dcf79d6e70d625
hamzabashir1268@gmail.com <hamzabashir1268@gmail.com>
2023-12-19 23:00:41 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./app.scss"
+
+const App = () => {
+  return <div>
+    <section>Hero</section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Hero</section>
+    <section>Hero</section>
+    <section>Hero</section>
+    <section>Hero</section>
+    <section>Hero</section>
+  </div>;
+};
+
+export default App;
